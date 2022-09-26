5282196ad8b6fc1cc98f58ad2239b0e893580baa
Volodymyr Volynets <volodymyr.volynets@student.griffith.ie>
2022-09-26 17:09:40 +0100

    Initial Commit

---
+++ CatGram/CatGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatGramApp.swift
+//  CatGram
+//
+//  Created by Vova on 26/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
